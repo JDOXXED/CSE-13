f7415cf1e67eebef41b64b98203bd8619f6188e1
JDOXXED <jdoxxed@gmail.com>
2026-04-30 00:08:11 +0000

    Assigment3 Start

---
+++ Assignments/Assignment3/freq.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int getMSDigit(int x){
+    while (x /10 > 10)
+    {
+        x /= 10;
+    }
+    return x % 10;
+}
+
+int main(){
+    int num;
+    printf("Enter a number: ");
+    scanf("%d",&num);
+
+    printf("%d\n", getMSDigit(num));
+    
+
+
+    return 0;
+}
