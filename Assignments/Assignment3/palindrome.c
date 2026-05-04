2093b691b45554b67956feebb07adbdfd78faad0
JDOXXED <jdoxxed@gmail.com>
2026-05-04 23:01:55 +0000

    palindrome

--- Assignments/Assignment3/palindrome.c
+++ Assignments/Assignment3/palindrome.c
@@ -1,51 +1,53 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <string.h>
 
-/*
-removes spaces from string
--checks if character is a space
--if a space, moves to next character
--if a character, inputs character
-*/
-void remove_spaces(char *str){
-    int i = 0; j = 0;
+int main() {
+    char str[101];
+    char fmtstr[101];
+    printf("Enter a string (max 100 characters): ");
 
-    while (str[i]){
-        if (str[i] != " "){
-            str[j++] = str[i];
-        }
-        i++;
-    }
-    str[j] = '\0';
-}
+    /* TODO: read an input string until the new-line character is read. 
+             remove the new line character and check if the string is a palindrome.
+             your check should ignore all non alpha-numeric characters and case-sensitivity.
+             print if the string is a palindrome or not.
+             your print statements should match the sample output prints shown in the pdf. */
+    
+    scanf("%100[^\n]", str);
+    int j = 0;
 
-int palindrome(char *str){
-     /*
-    palindrome function
-    -uses string without spaces
-    -checks first char with last, then continues
-    -iterates #chars / 2
-    Examples:
-    rttr - 4 chars --> 2 iterations --> compare string[0] with string[3], compare string[1] with string[2]
-    rtr - 3 chars --> 1 iteration --> compare string[0] with string[2], string[1] is middle
+    /*
+    Formats string
+    - removes non alphanumeric characters
+    - makes string lowercase
     */
 
-    int lenstring = strlen(str);
-
-    for (int i = 0; i < ; i++;){
-        if (getchar(i)){
-
+    for (int i = 0; i < strlen(str); i++){
+        if (isalnum(str[i])){
+            fmtstr[j] = tolower(str[i]);
+            j++;
         }
     }
-}
 
-int main(){
-    char string[100];
+    /*
+    palindrome checker
+    - breaks string into two seperate strings, reversing order of one
+    - checks if ascii value is equivalent
+    - racecar --> string1 = rac, string2 = rac
+    */
 
-    printf("Enter a string (max 100 characters): ");
-    fgets(string, 100, stdin);
-    
-    i
+    char fhalf[51] = "", shalf[51] = "";
+
+    for (int i = 0; i < strlen(fmtstr)/2; i++){
+        j = (strlen(fmtstr) -1 - i);
+        fhalf[i] = fmtstr[i];
+        shalf[i] = fmtstr[j];
+    }
+    if (strcmp(fhalf, shalf) ==0){
+        printf("The string is a palindrome.\n");
+    } else {
+        printf("The string is not a palindrome.\n");
+    }
 
     return 0;
 }
